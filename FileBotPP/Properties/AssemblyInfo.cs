fc8c0c585db1f0f85c784a4405e2d8f68b01764d
dmzoneill <dmz.oneill@gmail.com>
2015-10-19 22:56:08 +0100

    shortcut ico

--- FileBotPP/Properties/AssemblyInfo.cs
+++ FileBotPP/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.10.19.2146")]
-[assembly: AssemblyFileVersion("2015.10.19.2146")]
+[assembly: AssemblyVersion("2015.10.19.2155")]
+[assembly: AssemblyFileVersion("2015.10.19.2155")]
